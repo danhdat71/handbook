06923207c5b71e55d1d5799af4a6fdebadc21fb3
Danh Đạt <datd@bap.jp>
2022-04-24 19:36:27 +0700

    Update : update

--- app/Http/Requests/StoreBookRequest.php
+++ app/Http/Requests/StoreBookRequest.php
@@ -15,8 +15,8 @@ class StoreBookRequest extends BaseRequest
     public function rules()
     {
         return [
-            'images' => 'max:20|min:1',
-            'images.*' => 'required|mimes:jpg,png,jpeg|max:2000',
+            'images' => 'max:10|min:1',
+            'images.*' => 'required|mimes:jpg,png,jpeg|max:10000',
         ];
     }
 
